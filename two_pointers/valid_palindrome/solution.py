5450828f048335da6e214096ac34fd3bc22ff106
miesnerjacob <miesner.jacob@gmail.com>
2025-01-26 14:08:47 -0500

    immplemented two-pointers solution for valid palindrome

--- two_pointers/valid_palindrome/solution.py
+++ two_pointers/valid_palindrome/solution.py
@@ -1,4 +1,13 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = ''.join(c for c in s if c.isalnum()).lower()
-        return s == s[::-1]
+        
+        left_pointer = 0
+        right_pointer = len(s) - 1
+        
+        while left_pointer < right_pointer:
+            if s[left_pointer] != s[right_pointer]:
+                return False
+            left_pointer += 1
+            right_pointer -= 1
+        return True
