829ae6114a35caccc674899930ff2a28f8be678b
miesnerjacob <miesner.jacob@gmail.com>
2025-03-03 21:36:46 -0500

    solution to is subsequence

---
+++ arrays_and_hashing/is_subsequence/solution.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return "".join(sorted(s)) in "".join(sorted(t))
+            
